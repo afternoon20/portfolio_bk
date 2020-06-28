1f89b12bdb19d07e9d396ba5af45a38e8d1d22b5
Takocheese <51657737+Takocheese@users.noreply.github.com>
2020-06-29 03:08:40 +0900

    [add] プロフィールとスライド追加

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ポートフォリオ
+
+## コーディング規約
+
+### Sass は入れ子にしない。ただし、hover や nth(n)-child、ライブラリのクラスのカスタマイズは除く。
+
+非推奨
+.block{
+display: block;
+
+.block\_\_elements{
+margin:0;
+}
+}
+
+推奨
+.block{
+display: block;
+}
+
+.block\_\_elements{
+margin:0;
+}
---
+++ js/script.js
@@ -0,0 +1,28 @@
+// onwheel.carousel
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    // autoplay: true,
+    margin: 16,
+    nav: true,
+    items: 6, //アイテムの数
+    responsive: {
+      //レスポンシブ対応
+
+      // ブレイクポイント 0以上
+      0: {
+        items: 3,
+      },
+
+      // ブレイクポイント 768以上
+      768: {
+        items: 6,
+      },
+      margin: 0,
+    },
+    loop: false,
+
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+  });
+});
+
+// アコーディオン
