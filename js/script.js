29721f6992da35d021541e71efac9b447efa485b
Takocheese <51657737+Takocheese@users.noreply.github.com>
2020-07-12 18:32:09 +0900

    [add] 経歴を追加

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // onwheel.carousel
 $(function () {
-  $(".owl-carousel").owlCarousel({
+  $('.owl-carousel').owlCarousel({
     autoplay: true,
     margin: 16,
     nav: true,
@@ -26,3 +26,23 @@ $(function () {
 });
 
 // アコーディオン
+$(function () {
+  $('.career-list__item-bottom').hide();
+  $('.career-list__item-toggle-close').hide();
+
+  $('.career-list__item-toggle-open').click(function () {
+    $(this).siblings('.career-list__item-toggle-close').show();
+    $(this).siblings('.career-list__item-bottom').slideDown('normal');
+    $(this).hide();
+  });
+
+  $('.career-list__item-toggle-close').click(function () {
+    $(this).siblings('.career-list__item-toggle-open').show();
+    $(this).siblings('.career-list__item-bottom').slideUp('normal');
+    $(this).hide();
+
+    // $(this).siblings('.career-list__item-bottom').slideUp();
+    // $(this).children('.career-list__item-icon').css('transform', 'rotate(0turn)');
+    $(this).hide();
+  });
+});
