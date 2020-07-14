2fdf765964c39cf95886bb294aa8cdd59376e3e5
Takocheese <51657737+Takocheese@users.noreply.github.com>
2020-07-15 00:13:16 +0900

    [add] ogp追加

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $(function () {
   $('.owl-carousel').owlCarousel({
     autoplay: true,
+    autoplayTimeout: 8000,
     margin: 16,
     nav: true,
     items: 6, //アイテムの数
@@ -73,4 +74,16 @@ $(function () {
 // ハンバーガーメニュー
 $(document).ready(function () {
   $('.drawer').drawer();
+});
+
+// スムーススクロール
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr('href');
+    var target = $(href == '#' || href == '' ? 'html' : href);
+    var position = target.offset().top;
+    $('html, body').animate({ scrollTop: position }, speed, 'swing');
+    return false;
+  });
 });
