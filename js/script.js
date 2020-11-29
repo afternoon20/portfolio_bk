fa0b4bda2c603b490cfc5d09db31719ab184e540
Takocheese <51657737+Takocheese@users.noreply.github.com>
2020-11-29 15:29:16 +0900

    ポートフォリオ用に修正

--- js/script.js
+++ js/script.js
@@ -1,53 +1,3 @@
-// onwheel.carousel
-$(function () {
-  $('.owl-carousel').owlCarousel({
-    autoplay: true,
-    autoplayTimeout: 8000,
-    margin: 16,
-    nav: true,
-    items: 6, //アイテムの数
-    responsive: {
-      //レスポンシブ対応
-
-      // ブレイクポイント 0以上
-      0: {
-        items: 3,
-      },
-
-      // ブレイクポイント 768以上
-      768: {
-        items: 6,
-      },
-      margin: 0,
-    },
-    loop: false,
-
-    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-  });
-});
-
-// アコーディオン
-$(function () {
-  $('.career-list__item-bottom').hide();
-  $('.career-list__item-toggle-close').hide();
-
-  $('.career-list__item-toggle-open').click(function () {
-    $(this).siblings('.career-list__item-toggle-close').show();
-    $(this).siblings('.career-list__item-bottom').slideDown('normal');
-    $(this).hide();
-  });
-
-  $('.career-list__item-toggle-close').click(function () {
-    $(this).siblings('.career-list__item-toggle-open').show();
-    $(this).siblings('.career-list__item-bottom').slideUp('normal');
-    $(this).hide();
-
-    // $(this).siblings('.career-list__item-bottom').slideUp();
-    // $(this).children('.career-list__item-icon').css('transform', 'rotate(0turn)');
-    $(this).hide();
-  });
-});
-
 // トップへ戻るボタン
 $(function () {
   var pagetop = $('#page-top');
@@ -76,6 +26,14 @@ $(document).ready(function () {
   $('.drawer').drawer();
 });
 
+$(function () {
+  $('.drawer-menu-item').click(function () {
+    $('.drawer').removeClass('drawer-open');
+    $('.drawer').removeClass('drawer-close');
+    $('.drawer').drawer();
+  });
+});
+
 // スムーススクロール
 $(function () {
   $('a[href^="#"]').click(function () {
@@ -91,7 +49,7 @@ $(function () {
 // Typed.js
 $(function () {
   var typed = new Typed('.mv-ttl--typed', {
-    strings: ['', 'Accounting &amp; <br class="br-sp" /> Tax Staff.', 'a freelance <br class="br-sp" /> Web Developer.'],
+    strings: ['', 'My name is Kyosuke Shimada. ', 'Willkommen in meinem Portfolio!', 'Welcome to My Portfolio! '],
     startDelay: 0,
     typeSpeed: 50,
     backDelay: 2000,
